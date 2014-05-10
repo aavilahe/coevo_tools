6c5d37fc950dfcfe7e6eee1f896220b384a1331b
Aram Avila-Herrera <Aram.Avila-Herrera@ucsf.edu>
2014-05-10 12:43:29 -0700

    specify NCPU on command line

--- wrappers/prunPlmDCA.sh
+++ wrappers/prunPlmDCA.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 # runPlmDCA.sh -- tell matlab to run plmDCA_symmetric() with default options
 
-if [ $# -ne 2 ]; then
-	echo "$0 in.fa out.plmDCA"
+if [ $# -ne 3 ]; then
+	echo "$0 in.fa out.plmDCA Ncpus"
 	exit 1
 fi
 
 IN="'${1}'"
 OUT="'${2}'"
 
-NCPU=8 # max workers allowed (physical cores)
+#NCPU=8 # max workers allowed (physical cores)
+NCPU="${3}"
 
 CALL="plmDCA_symmetric(${IN},${OUT},0.01,0.01,0.1,${NCPU}) ; quit"
 
