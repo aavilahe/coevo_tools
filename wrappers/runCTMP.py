ed9d06a143a18b691b415093ae0a7114195b9dde
Aram Avila-Herrera <Aram.Avila-Herrera@ucsf.edu>
2015-06-17 13:23:54 -0700

    makes wrapper print CTMP's stdout

--- wrappers/runCTMP.py
+++ wrappers/runCTMP.py
@@ -58,5 +58,5 @@ if __name__ == "__main__":
     cfg = sys.argv[1]
     cfg_d = json.load(open(cfg, 'r'))
     cmd = ctmp_commandline(cfg_d)
-    #print >>sys.stderr, "running %s" % ' '.join(cmd)
-    Popen(cmd, stdout=PIPE).communicate()
+    print >>sys.stderr, "running %s" % ' '.join(cmd)
+    Popen(cmd).communicate()
