49b98701d3f041c7d7613d82df186d3407649ad3
Aram Avila-Herrera <Aram.Avila-Herrera@ucsf.edu>
2015-06-17 15:39:56 -0700

    Automatically adds headers and fixes ncol -> nrow

--- wrappers/hyphy_support/trimconst.R
+++ wrappers/hyphy_support/trimconst.R
@@ -1,6 +1,8 @@
 #!/usr/bin/Rscript
 library(data.table)
 fn = commandArgs(TRUE)[1]
-dt = fread(fn, sep = ',', header = TRUE)
-dtfil = dt[,which(colSums(dt) %ni% c(0,ncol(dt))), with = FALSE]
+dt = fread(fn, sep = ',', header = FALSE)
+the_headers = as.character(0:(ncol(dt) - 1))
+setnames(dt, the_headers)
+dtfil = dt[,which(colSums(dt) %ni% c(0,nrow(dt))), with = FALSE]
 write.table(dtfil, file=paste(fn, '.fixed', sep=''), quote=F, sep=',', row.names=F, col.names=T)
